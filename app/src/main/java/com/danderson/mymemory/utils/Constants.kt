184b2e1d81d56c1e8056dd10eaaa730c68a194ac
David Anderson <david.anderson281@yahoo.co.uk>
2022-03-15 16:25:54 +0000

    Initial commit

---
+++ app/src/main/java/com/danderson/mymemory/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.danderson.mymemory.utils
+
+import com.danderson.mymemory.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_airplane,
+    R.drawable.ic_alarm,
+    R.drawable.ic_bolt,
+    R.drawable.ic_cart,
+    R.drawable.ic_face,
+    R.drawable.ic_cocktail,
+    R.drawable.ic_flower,
+    R.drawable.ic_music,
+    R.drawable.ic_phone,
+    R.drawable.ic_star,
+    R.drawable.ic_sun,
+    R.drawable.ic_work
+)
